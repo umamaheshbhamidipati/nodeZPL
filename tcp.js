bc55056d6bea098b4925879554f4425956d8968b
umamaheshwar bhamidipati <umamaheshwar.bhamidipati@xcubelabs.com>
2019-12-18 18:37:58 +0530

    tcp with webscokets added

--- tcp.js
+++ tcp.js
@@ -1,13 +1,50 @@
 var net = require('net');
+const WebSocket = require('ws');
+var webSocketServer = require('ws').Server;
 
-var server = net.createServer(function(socket) {
+var wss = new webSocketServer({ port: 8080 });
+
+// we are opening from here only
+var clientConnection = new WebSocket('ws://localhost:8080');
+clientConnection.onopen = function () {
+	console.log("Connected to the signaling server");
+};
+
+var server = net.createServer(function (socket) {
 	socket.write('Echo server\r\n');
-	socket.on('data', function(data){
-		console.log(data);
+	socket.on('data', function (data) {
+		// console.log(data);
 		textChunk = data.toString('utf8');
-		console.log(textChunk);
+		console.log("textChunk>>>>>>", textChunk);
+
+		clientConnection.send(JSON.stringify(textChunk));
+
 		socket.write(textChunk);
 	});
 });
+
+wss.on('connection', function (connection) {
+    console.log("user connected..");
+
+    connection.on('message', function (message) {
+
+        console.log("meesage>>>>>>>>", message);
+        //accepting only JSON messages
+        try {
+            data = JSON.parse(message);
+        } catch (e) {
+            console.log("Invalid JSON");
+            data = {};
+        }
+        sendAll(message);
+    })
+});
+
+function sendAll(message) {
+    let cl = Array.from(wss.clients);
+    for (var i = 0; i < cl.length; i++) {
+        cl[i].send(message);
+    }
+}
 
 server.listen(1337, '192.168.3.6');
